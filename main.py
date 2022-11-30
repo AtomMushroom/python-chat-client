4940bd57e6fa5682d8afa06d712ad9406f25b894
AtomMushroom <vasilyev.atom@gmail.com>
2022-11-30 23:14:25 +0300

    Сделан основной интерфейс, реализован переход через ScreenManager

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-from kivy.app import App
-from kivy.uix.button import Button
-from kivy.config import Config
-from kivy.uix.screenmanager import ScreenManager
-from kivy.properties import ObjectProperty
-from kivy.uix.gridlayout import GridLayout
 from kivymd.app import MDApp
+from kivy.config import Config
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 Config.set("graphics", "resizeable", "0")
-Config.set("graphics", "width", "480")
-Config.set("graphics", "height", "853")
+#Config.set("graphics", "width", "480") WTF
+#Config.set("graphics", "height", "853") WTF
+Window.size = (480, 853)
 
-class AuthScreen(GridLayout):
+class AuthScreen(Screen):
     pass
 
-class MainScreen(GridLayout):
+class MainScreen(Screen):
     pass
 
 class ClientApp(MDApp):
     def build(self):
-        return AuthScreen()
+        self.title = 'Python Chat'
+        self.sm = ScreenManager()
+
+        self.sm.add_widget(AuthScreen(name='auth'))
+        self.sm.add_widget(MainScreen(name='menu'))
+
+        return self.sm
 
 if __name__ == "__main__":
     ClientApp().run()
