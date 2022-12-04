6c968148d5b1d6c67b268427a80a4917deb763ad
AtomMushroom <vasilyev.atom@gmail.com>
2022-12-04 12:37:20 +0300

    Нормальная попытка сделать сервер

--- server/server.py
+++ server/server.py
@@ -1,5 +1,4 @@
 import threading
-import time
 
 from kivymd.app import MDApp
 from kivy.config import Config
@@ -13,37 +12,37 @@ Config.set("graphics", "resizeable", "0")
 Window.size = (480, 853)
 
 
+def listen():  # МЕТОД ДЛЯ ЕГОРА. МОЖНО НЕ СМОТРЕТЬ, НИГДЕ НЕ ИСПОЛЬЗУЕТСЯ.
+    quit = False
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((socket.gethostbyname(socket.gethostname()), 2222))
+    print(f'Слушаю на {socket.gethostbyname(socket.gethostname())}:2222')
+    clients = []
 
+    while not quit:
+        try:
+            data, addr = s.recvfrom(1024)
+
+            if addr not in clients:
+                clients.append(addr)
+            if "KILLED" in data.decode("utf-8"):
+                clients.remove(addr)
+
+            # print(addr[0] + "=" + str(addr[1]), end="")
+            print(data.decode("utf-8"))
+
+            for client in clients:
+                if addr != client:
+                    s.sendto(data, client)
+        except:
+            print("\n[ Чтука сломалась ]")
+            quit = True
+            break
+mainT = threading.Thread(target=listen, daemon=True)
+mainT.start()
 class Main(Screen):
-    def listen(self):  # МЕТОД ДЛЯ ЕГОРА. МОЖНО НЕ СМОТРЕТЬ, НИГДЕ НЕ ИСПОЛЬЗУЕТСЯ.
-        quit = False
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.bind((socket.gethostbyname(socket.gethostname()), 2222))
-        print(f'Слушаю на {socket.gethostbyname(socket.gethostname())}:2222')
-        clients = []
-
-        while not quit:
-            try:
-                data, addr = s.recvfrom(1024)
-
-                if addr not in clients:
-                    clients.append(addr)
-                if "KILLED" in data.decode("utf-8"):
-                    clients.remove(addr)
-
-                # print(addr[0] + "=" + str(addr[1]), end="")
-                print(data.decode("utf-8"))
-
-                for client in clients:
-                    if addr != client:
-                        s.sendto(data, client)
-            except:
-                print("\n[ Чтука сломалась ]")
-                quit = True
-                break
     def start(self):
         self.ids.lb.text = f"IP: {socket.gethostbyname(socket.gethostname())}"
-        threading.Thread(target=self.listen).start()
 class ServerApp(MDApp):
     def build(self):
         self.title = 'Python Server'
